1d6c326cde7c50aa999aa402cc5ad2cd13aa44cd
Nick Harding <100789789+colonelkablam@users.noreply.github.com>
2025-02-03 12:08:32 +0000

    add dates to tasks

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import moment from "moment";
 
 const app = express();
 const port = 3000;
+const date = new Date().toISOString().split('T')[0]; // Get YYYY-MM-DD format
 
 const db = new pg.Client({
   user: "postgres",
@@ -17,17 +19,17 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Not connected to the db :(" }, // default list
-];
-
-async function getItems() {
-  const result = await db.query("SELECT * FROM items WHERE completed = FALSE ORDER BY id ASC;");
+async function getItems(date) {
+  const result = await db.query("SELECT * FROM items WHERE completed = FALSE AND date = $1 ORDER BY id ASC;",
+    [date]
+  );
   return result.rows;
 }
 
-async function getCompletedItems() {
-  const result = await db.query("SELECT * FROM items WHERE completed = TRUE;");
+async function getCompletedItems(date) {
+  const result = await db.query("SELECT * FROM items WHERE completed = TRUE AND date = $1 ORDER BY id ASC;",
+    [date]
+  );
   return result.rows;
 }
 
@@ -46,12 +48,25 @@ async function editItem(id, newTitle) {
 }
 
 app.get("/", async (req, res) => {
-  items = await getItems();
-  const completedItems = await getCompletedItems();
+
+  // Get date from query param or default to today
+  let date = req.query.date ? moment(req.query.date, 'YYYY-MM-DD') : moment();
+  // Generate previous and next dates
+  let prevDate = date.clone().subtract(1, 'day').format('YYYY-MM-DD');
+  let nextDate = date.clone().add(1, 'day').format('YYYY-MM-DD');
+  // Title to display
+  let formattedDate = (date.isSame(moment(), 'day') ? "TODAY " : "") + date.format('dddd, D MMMM');
+
+  // fetch the data
+  const items = await getItems(date);
+  const completedItems = await getCompletedItems(date);
   const error = req.query.error; // Get error from query params
 
   res.render("index.ejs", {
-    listTitle: "Today",
+    date: date,
+    prevDate: prevDate,
+    nextDate: nextDate,
+    formattedDate: formattedDate,
     listItems: items,
     completedListItems: completedItems,
     error: error,
@@ -80,7 +95,8 @@ app.post("/edit", async (req, res) => {
 });
 
 app.post("/check-off", async (req, res) => {
-
+  console.log(req.body.checkOffItemId);
+  res.redirect("/");
 });
 
 
